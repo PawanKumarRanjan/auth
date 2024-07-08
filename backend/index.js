65a62db68779461de392b25aa8c5a4a1b8d0a971
Pawan Kumar Ranjan <145214435+PawanKumarRanjan@users.noreply.github.com>
2024-07-08 15:37:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin: ["https://deploy-auth-frontend.vercel.app/"],
+        origin: ["https://deploy-auth-frontend.vercel.app"],
         methods:["POST", "GET"],
         credentials: true
     }
